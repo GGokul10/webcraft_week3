a8382c5cfd3dd848888b7c00e37526594fad5a7c
GGokul10 <gokulcp5366@gmail.com>
2025-06-30 23:55:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: '/webcraft_week3/', 
+    plugins: [react()],
 })
